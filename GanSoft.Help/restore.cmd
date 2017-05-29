eda370d5b5e846c7db2b1ff444b90ae7cd50a6d8
Dervis Cabello <l2_creative@hotmail.com>
2017-05-29 16:34:11 -0400

    Lightbox y cambio de contenido

--- GanSoft.Help/restore.cmd
+++ GanSoft.Help/restore.cmd
@@ -4,8 +4,5 @@ npm install -g bower
 pause
 echo Restore packages
 mkdir components
-bower install jquery
-bower install bootstrap
-bower install font-awesome
-bower install jasny-bootstrap
+bower update
 pause
